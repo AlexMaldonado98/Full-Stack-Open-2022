270cef9f6dba0ae38120ff9aa3b4a61e13994ec8
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2022-07-20 12:55:16 -0500

    exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,16 +6,25 @@ const Members = ({ persons }) => {
 
 };
 
+const Filter = ({ handleFilter, filter, filterPersons }) => {
+  return (
+    <div>
+      <input type={'text'} onChange={handleFilter} value={filter} />
+      {filterPersons.map((person) => <p key={person.name}>{person.name}</p>)}
+    </div>
+  )
+}
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      phone: 9341321421
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('');
   const [newPhone, setPhone] = useState('');
+  const [filter, setFilter] = useState('');
 
   const handleChange = (e) => {
     setNewName(e.target.value);
@@ -30,23 +39,32 @@ const App = () => {
     const same = persons.find((person) => {
       return person.name === newName
     });
-    console.log(same);
 
     if (same) {
       alert(`${same.name} is already added to phonebook`);
-    } else if (newName === ''){
+    } else if (newName === '') {
       alert('You have to fill the name field')
-    }else{
+    } else {
       setPersons(persons.concat({ name: newName, phone: newPhone }));
       setNewName('');
+      setPhone('');
     }
 
   }
+
+  const handleFilter = (e) => {
+    setFilter(e.target.value);
+  }
+
+  const wantedPerson = filter === '' ? [] : persons.filter((person) => person.name.toLocaleLowerCase().indexOf(filter.toLocaleLowerCase()) > -1);
 
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>filter shown with</p>
+      <Filter handleFilter={handleFilter} filterPersons={wantedPerson} value={filter} />
+      <h2>add a new</h2>
       <form onSubmit={handleAdd}>
         <div>
           name : <input type={'text'} onChange={handleChange} value={newName} /> <br></br>
