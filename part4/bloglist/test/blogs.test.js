fcbd3cb65c0d05f043ed12df1bb368c044d7bfa6
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2022-08-01 17:52:55 -0500

    exercise 4.5 and edit 4.4

--- part4/bloglist/test/blogs.test.js
+++ part4/bloglist/test/blogs.test.js
@@ -1,4 +1,66 @@
-const { dummy, totalLikes } = require('../utils/list_helper');
+const { dummy, totalLikes, favoriteBlog } = require('../utils/list_helper');
+
+const listWithOneBlog = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    }
+];
+
+const blogs = [
+    {
+        _id: '5a422a851b54a676234d17f7',
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 7,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: '5a422b3a1b54a676234d17f9',
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+        likes: 12,
+        __v: 0
+    },
+    {
+        _id: '5a422b891b54a676234d17fa',
+        title: 'First class tests',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+        likes: 10,
+        __v: 0
+    },
+    {
+        _id: '5a422ba71b54a676234d17fb',
+        title: 'TDD harms architecture',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+        likes: 0,
+        __v: 0
+    },
+    {
+        _id: '5a422bc61b54a676234d17fc',
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+        likes: 2,
+        __v: 0
+    }
+];
 
 test('dummy returns one', () => {
     const blogs = [];
@@ -8,35 +70,39 @@ test('dummy returns one', () => {
 });
 
 describe('total likes', () => {
-    const listWithOneBlog = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 5,
-            __v: 0
-        },
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 5,
-            __v: 0
-        },
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 5,
-            __v: 0
-        }
-    ];
+    const empty = [];
+
+    test('of empty list is zero', () => {
+        const result = totalLikes(empty);
+        expect(result).toBe(0);
+    });
 
     test('when list has only one blog, equals the likes of that', () => {
         const result = totalLikes(listWithOneBlog);
-        expect(result).toBe(listWithOneBlog.reduce((a,b) => a+b.likes,0));
+        expect(result).toBe(listWithOneBlog[0].likes);
+    });
+
+    test('of a bigger list is calculated right', () => {
+        const result = totalLikes(blogs);
+        expect(result).toBe(blogs.reduce((a,b) => a + b.likes,0));
+    });
+});
+
+describe('favorite blog', () => {
+
+    test('find the most voted blog', () => {
+        const result = favoriteBlog(blogs);
+        expect(result).toEqual(
+            {
+                title: 'Canonical string reduction',
+                author: 'Edsger W. Dijkstra',
+                likes: 12
+            }
+        );
+    });
+
+    test('return zero when array is empty', () => {
+        const result = favoriteBlog([]);
+        expect(result).toEqual(0);
     });
 });
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -7,12 +7,28 @@ const dummy = (Array) => {
 };
 
 const totalLikes = (Array) => {
+
+    if(Array.length === 0) return 0;
+
     const likes = Array.map(blog => blog.likes);
-    console.log(likes);
     return likes.reduce((a,b) => a + b,0);
+};
+
+const favoriteBlog = (Array) => {
+
+    if(Array.length === 0 ) return 0;
+
+    const favoriteBlog = Array.reduce((a,b) => a.likes > b.likes ? a : b);
+
+    return {
+        title: favoriteBlog.title,
+        author: favoriteBlog.author,
+        likes: favoriteBlog.likes
+    };
 };
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 };
