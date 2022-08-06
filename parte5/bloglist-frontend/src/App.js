93b44d6a27ffcb488dd9a4648a72de01aae9eb42
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2022-08-06 01:06:28 -0500

    exercise 5.5 and 5.6

--- parte5/bloglist-frontend/src/App.js
+++ parte5/bloglist-frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Blog from './components/Blog';
 import blogService from './services/blogs';
 import { loginUser } from './services/login';
@@ -6,91 +6,98 @@ import { LoginForm } from './components/LoginForm';
 import { BlogForm } from './components/BlogForm';
 import { Notifications } from './components/Notification';
 import './App.css';
+import Togglable from './components/Togglable';
+
 
 const App = () => {
     const [blogs, setBlogs] = useState([]);
     const [user, setUser] = useState(null);
     const [message, setMessage] = useState(null);
-
+    
+    const toggleRef = useRef();
+    
     useEffect(() => {
         blogService.getAll().then(blogs =>
-            setBlogs( blogs )
-        );  
+            setBlogs(blogs)
+        );
     }, []);
 
     useEffect(() => {
         const userCredentials = window.localStorage.getItem('userCredentials');
-        if(userCredentials){
+        if (userCredentials) {
             const user = JSON.parse(userCredentials);
             blogService.setToken(user.token);
             setUser(user);
         }
-    },[]);
+    }, []);
 
-    const handleLogin = async (userName,password) => {
-        if(userName && password !== ''){
+    const handleLogin = async (userName, password) => {
+        if (userName && password !== '') {
             try {
-                const user = await loginUser({username:userName, passwordHash:password});
-                window.localStorage.setItem('userCredentials',JSON.stringify(user));
+                const user = await loginUser({ username: userName, passwordHash: password });
+                window.localStorage.setItem('userCredentials', JSON.stringify(user));
                 blogService.setToken(user.token);
                 setUser(user);
                 setMessage('Login success');
                 setTimeout(() => {
                     setMessage(null);
-                },3000);
+                }, 3000);
             } catch (error) {
                 setMessage(`[ERROR] ${error.response.data.error}`);
                 setTimeout(() => {
                     setMessage(null);
-                },3000);
+                }, 3000);
             }
-        }else{
+        } else {
             setMessage('[ERROR] You need to fill in all the fields');
             setTimeout(() => {
                 setMessage(null);
-            },5000);
-            
+            }, 5000);
+
         }
     };
-    
+
     const handleLogout = (event) => {
         event.preventDefault();
         window.localStorage.removeItem('userCredentials');
         setUser(null);
     };
 
-    const handleNewBlog = async (title,author,url) => {
+    const handleNewBlog = async (title, author, url) => {
         try {
-            const response = await blogService.create({title,author,url});
-            setBlogs([...blogs,response]);
+            const response = await blogService.create({ title, author, url });
+            setBlogs([...blogs, response]);
             setMessage(`A new blog ${response.title} by ${response.author} added`);
+            toggleRef.current.toggleVisibility();
             setTimeout(() => {
                 setMessage(null);
-            },5000);
+            }, 5000);
         } catch (error) {
             setMessage('[ERROR] the title and url is required');
             setTimeout(() => {
                 setMessage(null);
-            },5000);
+            }, 5000);
         }
     };
 
-    if(user === null){
-        return(
+    if (user === null) {
+        return (
             <>
-                <Notifications message={message} /> 
+                <Notifications message={message} />
                 <LoginForm handleLogin={handleLogin} />
             </>
         );
     }
-    
+
     return (
         <div>
-            <Notifications message={message} /> 
+            <Notifications message={message} />
             <span>{`user: ${user.username} logged in`}</span>
             <button onClick={handleLogout}>logout</button>
             <h1>create new</h1>
-            <BlogForm handleNewBlog={handleNewBlog} />
+            <Togglable button={'New blog'} ref={toggleRef} >
+                <BlogForm handleNewBlog={handleNewBlog} />
+            </Togglable>
             <h2>blogs</h2>
             {blogs.map(blog =>
                 <Blog key={blog.id} blog={blog} />
