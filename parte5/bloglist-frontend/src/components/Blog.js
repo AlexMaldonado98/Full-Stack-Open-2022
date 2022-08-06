36063de928f7e29eeceede78b819b730e1b81bf2
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2022-08-06 13:23:43 -0500

    exercise 5.7

--- parte5/bloglist-frontend/src/components/Blog.js
+++ parte5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,26 @@
-const Blog = ({blog}) => (
-    <div>
-        {`Title: ${blog.title} Author: ${blog.author}`}
-    </div>  
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+    const [visible, setVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        setVisible(!visible);
+    };
+
+    return (
+        <div className='container-blog'>
+            {`Title: ${blog.title}`}
+            <button style={{marginLeft: '10px'}} onClick={toggleVisibility} >{visible ? 'hide' : 'show'}</button>
+            {visible === true ? (
+                <>
+                    <p>{`URL: ${blog.url}`}</p>
+                    <p>{`Likes: ${blog.likes}`} <button>like</button>  </p>
+                    <p>{`Author: ${blog.author}`}</p>
+                </>
+            )
+                : ''}
+        </div>
+    );
+};
 
 export default Blog;
