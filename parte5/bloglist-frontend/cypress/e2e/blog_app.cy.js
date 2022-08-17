1b9f8fa56c072b7dd9f685041fe97a68c5cd5a06
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2022-08-16 21:47:15 -0500

    exercise 5.18

--- parte5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ parte5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,11 +1,32 @@
 describe('Blog app', () => {
     beforeEach(() => {
         cy.request('POST','http://localhost:3006/api/testing/reset');
+        cy.request('POST','http://localhost:3006/api/users',{
+            username:'alextest',
+            name: 'alex',
+            passwordHash: 'alexpass'
+        });
         cy.visit('http://localhost:3000');
     });
     it('Login form is shown',() => {
         cy.contains('log in to application');
         cy.contains('Username');
         cy.contains('Password');
+    });
+
+    describe('login', () => {
+        it('succeeds with correct credentials',() => {
+            cy.get('input[name=Username]').type('alextest');
+            cy.get('input[name=Password]').type('alexpass');
+            cy.get('#login-button').click();
+            cy.contains('user: alextest logged in');
+        });
+
+        it('succeeds with incorrect credentials',() => {
+            cy.get('input[name=Username]').type('alextest');
+            cy.get('input[name=Password]').type('badpassword');
+            cy.get('#login-button').click();
+            cy.contains('the username or password is incorrect.');
+        });
     });
 });
--- parte5/bloglist-frontend/src/components/LoginForm.js
+++ parte5/bloglist-frontend/src/components/LoginForm.js
@@ -32,7 +32,7 @@ export const LoginForm = ({handleLogin}) => {
                     onChange={({ target }) => setPassword(target.value)}
                 />
             </div>
-            <button type='submit' >login</button>
+            <button type='submit' id='login-button' >login</button>
         </form>
     );
 };
