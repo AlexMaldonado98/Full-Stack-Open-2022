97ae03075ef85432b599e6bf7a5919a018397232
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2022-08-19 13:36:59 -0500

    exercise 6.2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -6,21 +6,21 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
+  const handleAction = (type) => {
     store.dispatch({
-      type: 'GOOD'
+      type
     })
   }
 
   return (
     <div>
-      <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={() => handleAction('GOOD') }>good</button>
+      <button onClick={() => handleAction('OK')}>ok</button>
+      <button onClick={() => handleAction('BAD')}>bad</button>
+      <button onClick={() => handleAction('ZERO')}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
