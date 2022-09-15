573545e015a2e3b3395eeb50060b41d32e0af46c
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2022-09-15 17:58:39 -0500

    exercises 8.5

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -111,7 +111,7 @@ const typeDefs = gql`
     type Query {
         bookCount: Int!
         authorCount: Int!
-        allBooks( author: String ): [Book!]!
+        allBooks( author: String, genres: String ): [Book!]!
         allAuthors: [Author!]!
     }
 `;
@@ -120,7 +120,18 @@ const resolvers = {
     Query: {
         bookCount: () => books.length,
         authorCount: () => authors.length,
-        allBooks: (root,args) => args.author ? books.filter(book => book.author === args.author) : books,
+        allBooks: (root,args) => {
+            if(args.author && args.genres){
+                return books.filter(book => book.author === args.author && book.genres.includes(args.genres));
+            }else if (args.genres){
+                return books.filter(book => book.genres.includes(args.genres));
+            }else if(args.author){
+                return books.filter(book => book.author === args.author);
+                
+            }else{
+                return books;
+            }
+        },
         allAuthors: () => authors
     },
     Author: {
