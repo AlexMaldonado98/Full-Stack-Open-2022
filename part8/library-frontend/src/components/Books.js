29cad044dad944f11f03a316738b93316fd622b3
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2022-09-18 14:12:55 -0500

    exercise 8.9

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,17 +1,25 @@
+import { useQuery } from "@apollo/client";
+import { ALL_BOOKS } from "../authors/queries";
+
 const Books = (props) => {
+
+  const {data, loading} = useQuery(ALL_BOOKS);
+
   if (!props.show) {
     return null
   }
+  
+  if (loading) return <div>loading...</div>
 
-  const books = []
+  const books = data.allBooks || []
 
   return (
     <div>
       <h2>books</h2>
 
-      <table>
+      <table >
         <tbody>
-          <tr>
+          <tr >
             <th></th>
             <th>author</th>
             <th>published</th>
