57d99b04298f24fe4a49abb9efd791b891799d47
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2022-10-22 13:11:51 -0500

    exercise 9.9 and remastered repositorio

--- part9/patientor/backend/index.ts
+++ part9/patientor/backend/index.ts
@@ -1,15 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 
 app.get("/api/ping",(_req,res) => {
     res.status(200).send("pong");
 });
 
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT,() => {
     console.log(`server running on port ${PORT}`);
 });
